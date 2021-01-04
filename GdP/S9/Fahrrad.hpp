809ce84887ef0234ddfc4a136710ebeb423c7b9f
ZetaTom <70907959+ZetaTom@users.noreply.github.com>
2021-01-04 17:41:24 +0100

    Rechtschreibfehler behoben

--- GdP/S9/Fahrrad.hpp
+++ GdP/S9/Fahrrad.hpp
@@ -3,15 +3,15 @@
 class Fahrrad {
 	//Methoden
 	public:						// "Öffentlich" → Zugriff von außerhalb möglich
-		Fahrrad(int kettenblatt, int rizel, double raddurchmesser);
+		Fahrrad(int kettenblatt, int ritzel, double raddurchmesser);
 		Fahrrad();
-		double bestimmeRadumfang();
-		double bestimmeEntfaltung();
-		double bestimmeGeschwindigkeit(const int& TiM);
+		double bestimmeRadumfang();						// U in m
+		double bestimmeEntfaltung();					// e in m
+		double bestimmeGeschwindigkeit(const int& TiM);	// T in 1/min
 
 	//Attribute
 	private:					// Zugriff nur innerhalb der Klasse möglich
 		int kettenblatt;		//< Anzahl der Zähne des Zahnkranzes (vorne)
-		int rizel;				//< Anzahl der Zähne des Ritzels (hinten)
+		int ritzel;				//< Anzahl der Zähne des Ritzels (hinten)
 		double raddurchmesser;	//< Raddurchmesser in Metern [m]
 };
